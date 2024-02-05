e3abb8b958446d8966ecb0c30c8c2b8bc66d2082
rikimaru35kg <rikimaru35kg@gmail.com>
2024-02-05 13:23:34 +0900

    [update] PlaySound class depending on day

--- main.py
+++ main.py
@@ -10,14 +10,20 @@ def main():
     food2 = utils.PlaySound("11:30:01", const.WAV_FOOD)
     time_1200 = utils.PlaySound("12:00:00", const.WAV_1200)
     time_notification1 = utils.PlaySound("12:00:01", const.WAV_TIMENOTIFICATION)
+    time_1435 = utils.PlaySound("14:35:00", const.WAV_1435, day=[2])
+    pickup_ko = utils.PlaySound("14:35:01", const.WAV_PICKUPKO, day=[2])
     time_1800 = utils.PlaySound("18:00:00", const.WAV_1800)
     food3 = utils.PlaySound("18:00:01", const.WAV_FOOD)
     time_2000 = utils.PlaySound("20:00:00", const.WAV_2000)
     homework = utils.PlaySound("20:00:00", const.WAV_HOMEWORK)
     time_2100 = utils.PlaySound("21:00:00", const.WAV_2100)
-    time_notification2 = utils.PlaySound("21:00:01", const.WAV_TIMENOTIFICATION)
+    time_notification2 = utils.PlaySound("21:00:01", const.WAV_TIMENOTIFICATION, day=[1, 4, 5])
+    cardboard = utils.PlaySound("21:00:01", const.WAV_CARDBOARD, day=[0])
+    flammable = utils.PlaySound("21:00:01", const.WAV_FLAMMABLE, day=[2, 6])
+    inflammable = utils.PlaySound("21:00:01", const.WAV_INFLAMMABLE, day=[3])
     time_2200 = utils.PlaySound("22:00:00", const.WAV_2200)
     sleep = utils.PlaySound("22:00:00", const.WAV_SLEEP)
+
     while (True):
         time_0630.playsound()
         food1.playsound()
@@ -31,9 +37,13 @@ def main():
         time_2000.playsound()
         homework.playsound()
         time_notification2.playsound()
+        cardboard.playsound()
+        flammable.playsound()
+        inflammable.playsound()
         time_2100.playsound()
         time_2200.playsound()
         sleep.playsound()
+
 
 if __name__ == '__main__':
     main()
--- utils/play_sound.py
+++ utils/play_sound.py
@@ -11,14 +11,26 @@ import const
 class PlaySound:
     waiting = True
     playtime = None; playfile = None
-    def __init__(self, playtime, playfile):
+    day = []
+    def __init__(self, playtime, playfile, day=[]):
+        """Initialize PlaySound class
+        Args:
+            playtime (str): e.g. "06:55:00"
+            playfile (str): filename of wav file
+            day (list[int]): 0(Mon),1(Tue),...,6(Sun), eg) [0, 3, 6]
+        """
         self.playtime = playtime
         self.playfile = playfile
+        self.day = day
     def playsound(self):
+        """Play sound if the conditions are met"""
         dt_now = dt.now()
         t_notify = dt.time(dt.strptime(self.playtime, '%H:%M:%S'))
         dt_notify = dt.combine(date.today(), t_notify)
         dt_notify_buf = dt_notify + td(seconds=const.TIME_BUFFER)
+
+        day = dt_now.weekday()
+        if (len(self.day)>0 and day not in self.day): return
 
         if (self.waiting):
             if (dt_now < dt_notify): return
