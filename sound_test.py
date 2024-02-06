afee4d21a48148eff9dc95803594232884ad6167
rikimaru35kg <rikimaru35kg@gmail.com>
2024-02-06 09:19:13 +0900

    [update] sound_test.py for all wav files

--- sound_test.py
+++ sound_test.py
@@ -1,7 +1,12 @@
+from glob import glob
 import utils
 import const
 
 from datetime import datetime as dt
 
-test = utils.PlaySound(dt.now().strftime("%H:%M:%S"), const.WAV_FOOD)
-test.playsound()
+filelist = [f for f in glob("voice/*.wav")]
+filelist.sort()
+for f in filelist:
+    print(f"\n{f}")
+    test = utils.PlaySound(dt.now().strftime("%H:%M:%S"), f)
+    test.playsound()
