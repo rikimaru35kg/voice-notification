bbaa92e183f5da6ca17ce3b5b395a1bf16c48e65
rikimaru35kg <rikimaru35kg@gmail.com>
2024-02-19 10:36:30 +0900

    [update] change some voices (remove the word Ko)

--- const/const.py
+++ const/const.py
@@ -7,13 +7,15 @@ TIME_BUFFER = 60  # [sec]
 #======================================================
 # voice files' path
 #======================================================
-WAV_0730 = r'voice/001_ずんだもん（ノーマル）_7時30分なのだ.wav'
-WAV_TUTORING = r'voice/001_ずんだもん（ノーマル）_そろそろ塾なのだ。….wav'
-WAV_IBOKORORI = r'voice/002_ずんだもん（ノーマル）_こうのイボコロリを….wav'
+WAV_IBOKORORI = r'voice/001_ずんだもん（ノーマル）_イボコロリを忘れず….wav'
+WAV_PICKUPKO = r'voice/002_ずんだもん（ノーマル）_お迎えに行くのに、….wav'
 WAV_FOOD = r'voice/002_ずんだもん（ノーマル）_ズーのごはんのふや….wav'
+WAV_TUTORING = r'voice/003_ずんだもん（ノーマル）_そろそろ塾なのだ。….wav'
+WAV_0720 = r'voice/004_ずんだもん（ノーマル）_7時20分なのだ.wav'
 WAV_1130 = r'voice/004_ずんだもん（ノーマル）_１１時３０分なのだ.wav'
 WAV_1435 = r'voice/005_ずんだもん（ノーマル）_２時３５分なのだ.wav'
-WAV_PICKUPKO = r'voice/006_ずんだもん（ノーマル）_こうを迎えにいくの….wav'
+WAV_1700 = r'voice/005_ずんだもん（ノーマル）_5時なのだ.wav'
+WAV_2130 = r'voice/006_ずんだもん（ノーマル）_9時30分なのだ.wav'
 WAV_1800 = r'voice/007_ずんだもん（ノーマル）_６時なのだ.wav'
 WAV_2000 = r'voice/008_ずんだもん（ノーマル）_８時なのだ.wav'
 WAV_HOMEWORK = r'voice/009_ずんだもん（ノーマル）_そろそろ宿題を始め….wav'
@@ -32,8 +34,3 @@ WAV_0655_SMALL = r'voice/small_003_ずんだもん（ノーマル）_6時55分�
 WAV_WAKEUP_SMALL = r'voice/small_004_ずんだもん（ノーマル）_そろそろ起きるのだ.wav'
 WAV_2200_SMALL = r'voice/small_005_ずんだもん（ノーマル）_10時なのだ.wav'
 WAV_SLEEP_SMALL = r'voice/small_006_ずんだもん（ノーマル）_そろそろ寝るのだ.wav'
-#--------------------------------------------------------
-# archive 
-# WAV_0655 = r'voice/001_ずんだもん（ノーマル）_６時５５分なのだ。….wav'
-# WAV_0630 = r'voice/003_ずんだもん（ノーマル）_６時３０分なのだ.wav'
-# WAV_SLEEP = r'voice/014_ずんだもん（ノーマル）_そろそろ寝るのだ.wav'
